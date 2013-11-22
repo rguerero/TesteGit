498fed11a22f6be8b36e5787e5ff4dc629be7290
Rafael <Rafael@Thorn.com>
2013-11-22 19:10:08 -0200

    programa alterado por fulano

--- TesteGit/src/Principal.java
+++ TesteGit/src/Principal.java
@@ -1,7 +1,9 @@
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Primeira versão");
+		System.out.println("Terceira versão");
+		
+		System.out.println("Editado por Fulano");
 
 	}
 
